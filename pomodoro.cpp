0ba8c9581052a8c6fde69a12b3e5a2c9c759134d
Liam Wohlstedt <105879454+LwwDev@users.noreply.github.com>
2025-12-12 15:00:16 +0100

    Add files via upload

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -5,12 +5,8 @@
 #include <thread>
 
 void Menu();
-// void startTimer while timer is ongoing give option to stop end continue etc
-// break short/long
-// Exit
-//
-void startTimer();
-void Clear();
+void startTimer(int minutes);
+void takeBreak();
 
 int main() {
   Menu();
@@ -20,7 +16,7 @@ int main() {
 
 void Menu() {
   bool stop = false;
-  while (!stop) {
+  do {
     std::cout << "============================\n";
     std::cout << "====== Pomodoro Timer ======\n";
     std::cout << "============================\n\n";
@@ -33,7 +29,10 @@ void Menu() {
 
     switch (menuChoice) {
     case 1:
-      startTimer();
+      startTimer(25);
+      break;
+    case 2:
+      takeBreak();
       break;
     case 3:
       std::cout << "Exiting program...";
@@ -44,16 +43,44 @@ void Menu() {
       std::cout << "pick something from the menu please\n\n";
       break;
     }
+  } while (!stop);
+}
+
+void takeBreak() {
+  int breakChoice;
+  std::cout << "How long break would you like to take?: \n\n"
+            << "1. 5 minutes\n"
+            << "2. 15 minutes\n"
+            << "3. Custom time\n\n";
+  std::cin >> breakChoice;
+
+  switch (breakChoice) {
+  case 1:
+    startTimer(5);
+    break;
+  case 2:
+    startTimer(15);
+    break;
+  case 3: {
+    int custom;
+    std::cout << "Choose custom time (minutes): \n";
+    std::cin >> custom;
+    startTimer(custom);
+    break;
+  }
+  default:
+    std::cout << "choose a valid option\n";
+    break;
   }
 }
-void startTimer() {
-  int minutes = 25;
+
+void startTimer(int minutes) {
   std::cout << "Pomodoro timer started for " << minutes << " minutes!\n";
 
   for (int i = minutes * 60; i > 0; i--) {
     int min = i / 60;
     int sec = i % 60;
-    std::cout << "\r" << min << "m " << sec << "s remaning " << std::flush;
+    std::cout << "\r" << min << "m " << sec << "s remaining " << std::flush;
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
   std::cout << "\nTime's up! Take a break!\n";
